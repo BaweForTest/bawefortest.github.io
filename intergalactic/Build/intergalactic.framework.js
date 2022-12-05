4d945cd879320f6a2ce6e34c92e112a681eb8b93
Bawenang Rukmoko <bawenang@solecode.id>
2022-12-05 17:34:25 +0700

    Intergalactic 75% done

--- intergalactic/Build/intergalactic.framework.js
+++ intergalactic/Build/intergalactic.framework.js
@@ -1293,7 +1293,7 @@ function _emscripten_asm_const_id(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 2496880;
+STATICTOP = STATIC_BASE + 2543456;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AIScriptingClasses_cpp();
@@ -3367,7 +3367,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 2496880;
+var STATIC_BUMP = 2543456;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
@@ -15467,8 +15467,8 @@ function nullFunc_vjji(x) {
  err("Build with ASSERTIONS=2 for more info.");
  abort(x);
 }
-Module["wasmTableSize"] = 56303;
-Module["wasmMaxTableSize"] = 56303;
+Module["wasmTableSize"] = 56331;
+Module["wasmMaxTableSize"] = 56331;
 function invoke_dddi(index, a1, a2, a3) {
  var sp = stackSave();
  try {
